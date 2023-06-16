4a5ed5a191d35632c01f247819b35a77e738c802
RealByter <justavishay@gmail.com>
2023-06-16 20:09:04 +0300

    feat: show an error with a modal on login/signup error

--- src/components/authentication/Login.tsx
+++ src/components/authentication/Login.tsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import Input from "../general/Input";
 import { useAuth } from "reactfire";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { FirebaseError } from "firebase/app";
+import LoadingSpinner from "../general/LoadingSpinner";
+import Modal from "../general/Modal";
 
 interface FormData {
   email: string;
@@ -9,6 +16,9 @@ interface FormData {
 }
 
 const Login = () => {
+  const [login, setLogin] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [modal, setModal] = useState("");
   const auth = useAuth();
   const {
     register,
@@ -18,30 +28,78 @@ const Login = () => {
   } = useForm<FormData>();
 
   const onSubmit = async (data: FormData) => {
-    await signInWithEmailAndPassword(auth, data.email, data.password);
+    setLoading(true);
+    if (login) {
+      signInWithEmailAndPassword(auth, data.email, data.password)
+        .catch((e) => {
+          if (e instanceof FirebaseError) {
+            if (e.code === "auth/user-not-found")
+              setModal("Couldn't find the user. Try another one");
+            else if (e.code === "auth/wrong-password")
+              setModal("Incorrect password. Please try again");
+          }
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    } else {
+      createUserWithEmailAndPassword(auth, data.email, data.password)
+        .catch((e) => {
+          if (e instanceof FirebaseError) {
+            if (e.code === "auth/email-already-in-use")
+              setModal("Email already in use. Try to log in?");
+          }
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Input
-          register={register}
-          rules={{ required: "this is required" }}
-          placeholder="Enter an email"
-          label="Email"
-          errorMessage={errors.email?.message}
-        />
-        <Input
-          type="password"
-          register={register}
-          rules={{ required: "this is required" }}
-          placeholder="Enter a password"
-          label="Password"
-          errorMessage={errors.email?.message}
-        />
-        <button type="submit">LOGIN</button>
-      </form>
-    </div>
+    <>
+      <LoadingSpinner loading={loading} />
+      <Modal
+        title={login ? "Login error" : "Sign up error"}
+        description={modal}
+        onClose={() => {
+          setModal("");
+        }}
+        open={!!modal}
+      />
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            register={register}
+            rules={{
+              required: "this is required",
+              pattern: {
+                value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}/,
+                message: "must be a valid email address",
+              },
+            }}
+            placeholder="Enter an email"
+            label="Email"
+            errorMessage={errors.email?.message}
+          />
+          <Input
+            type="password"
+            register={register}
+            rules={{ required: "this is required" }}
+            placeholder="Enter a password"
+            label="Password"
+            errorMessage={errors.password?.message}
+          />
+          <button type="submit">{login ? "Login" : "Signup"}</button>
+          <p>
+            {login ? "Don't have an account?" : "Already have an account?"}{" "}
+            <a onClick={() => setLogin((oldState) => !oldState)}>
+              {login ? "Signup" : "Login"}
+            </a>
+          </p>
+        </form>
+      </div>
+    </>
   );
 };
 
