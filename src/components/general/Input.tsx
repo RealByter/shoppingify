851390ca284e40f72077149b388f961c765a80d9
RealByter <justavishay@gmail.com>
2023-06-16 16:00:05 +0300

    feat: create the Input component

---
+++ src/components/general/Input.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { RegisterOptions } from "react-hook-form";
+
+interface Props {
+  type: string;
+  placeholder: string;
+  register: any;
+  label: string;
+  errorMessage: string;
+  rules: RegisterOptions;
+}
+
+const Input: React.FC<Props> = ({
+  type = "text",
+  placeholder,
+  register,
+  label,
+  errorMessage,
+  rules,
+}) => {
+  return (
+    <div>
+      <label>
+        {label}
+        <input
+          type={type}
+          placeholder={placeholder}
+          {...(register && register(label, rules))}
+        />
+      </label>
+      <p>{errorMessage && `* ${errorMessage}`}</p>
+    </div>
+  );
+};
+
+export default Input;
