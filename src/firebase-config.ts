ba0bb303f5accec0cb996595837a0640cb6993a1
RealByter <justavishay@gmail.com>
2023-06-16 11:52:40 +0300

    feat: setup the firebase config

---
+++ src/firebase-config.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAltaAAbp3yed-PjevL5ASwWihJYeNCGU0",
+  authDomain: "shoppingify-1bd7f.firebaseapp.com",
+  projectId: "shoppingify-1bd7f",
+  storageBucket: "shoppingify-1bd7f.appspot.com",
+  messagingSenderId: "601698294825",
+  appId: "1:601698294825:web:6a0e15be24181c41d8d038",
+  measurementId: "G-BMQPZHF7V1",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
