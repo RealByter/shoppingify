9a3e978f2b0405389225f77645063c45ece54e74
RealByter <justavishay@gmail.com>
2023-06-17 17:25:40 +0300

    feat: revert routing changes

--- src/components/navigation/RouteSwitch.tsx
+++ src/components/navigation/RouteSwitch.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "../authentication/Login";
 import { useSigninCheck } from "reactfire";
 import NavBar from "./NavBar";
+import Items from "../items/Items";
 
 const RouteSwitch = () => {
   const { status, data: signInCheckResult } = useSigninCheck();
@@ -12,23 +13,25 @@ const RouteSwitch = () => {
   }
 
   return (
-    <BrowserRouter basename="/shoppingify">
+    <BrowserRouter>
       <div className="flex">
         {signInCheckResult.signedIn && <NavBar />}
-        <Routes>
-          {!signInCheckResult.signedIn && (
-            <Route path="/login" element={<Login />} />
-          )}
-          {signInCheckResult.signedIn && (
-            <Route path="/" element={<p>Logged in</p>} />
-          )}
-          <Route
-            path="*"
-            element={
-              <Navigate to={signInCheckResult.signedIn ? "/" : "/login"} />
-            }
-          />
-        </Routes>
+        <div className="grow">
+          <Routes>
+            {!signInCheckResult.signedIn && (
+              <Route path="/login" element={<Login />} />
+            )}
+            {signInCheckResult.signedIn && (
+              <Route path="/" element={<Items />} />
+            )}
+            <Route
+              path="*"
+              element={
+                <Navigate to={signInCheckResult.signedIn ? "/" : "/login"} />
+              }
+            />
+          </Routes>
+        </div>
       </div>
     </BrowserRouter>
   );
