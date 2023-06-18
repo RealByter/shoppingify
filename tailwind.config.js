e7b33a1048e81901d7341b27ff56cae0d6c5c1e7
RealByter <justavishay@gmail.com>
2023-06-19 00:55:11 +0300

    feat: install the headlessui teailwindcss plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
     extend: {
       colors: {
-        primary: '#F9A109'
-      }
+        primary: "#F9A109",
+      },
     },
   },
-  plugins: [],
-}
+  plugins: [require("@headlessui/tailwindcss")({ prefix: "ui" })],
+};
