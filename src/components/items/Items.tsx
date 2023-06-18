91306b7908cfbc479f96d3e01329ce2b6fd906ba
RealByter <justavishay@gmail.com>
2023-06-18 23:51:49 +0300

    feat: add a drawer create item form

--- src/components/items/Items.tsx
+++ src/components/items/Items.tsx
@@ -7,14 +7,11 @@ import Category from "./Category";
 import { DocumentData, collection, query, where } from "firebase/firestore";
 import LoadingSpinner from "../general/LoadingSpinner";
 import Item from "./Item.interface";
-
-const categories = [
-  "Fruit and vegetables",
-  "Meat and Fish",
-  "Beverages",
-  "Sweets",
-  "Pets",
-];
+import { useState } from "react";
+import SideDrawer from "../general/SideDrawer";
+import CreateItemButton from "./CreateItemButton";
+import categories from "./categories";
+import CreateItemForm from "./CreateItemForm";
 
 const Items = () => {
   const { data: signInCheckResult } = useSigninCheck();
@@ -27,25 +24,46 @@ const Items = () => {
   const { status, data: items } = useFirestoreCollectionData(itemsQuery, {
     idField: "id",
   });
+  const [isShowing, setIsShowing] = useState(false);
 
   if (status === "loading") {
     return <LoadingSpinner loading />;
   }
 
+  const checkIfItemExists = (name: string): boolean => {
+    return !!items.find((item) => item.name === name);
+  };
+
   return (
-    <div className="mx-auto max-w-5xl p-3">
-      {categories.map((category: string, index: number) => (
-        <Category
-          key={index}
-          name={category}
-          items={
-            items.filter(
-              (item: DocumentData) => item.category === category
-            ) as Item[]
-          }
+    <>
+      <div className="mx-auto max-w-3xl p-3">
+        {categories.map((category: string, index: number) => (
+          <Category
+            key={index}
+            name={category}
+            items={
+              items.filter(
+                (item: DocumentData) => item.category === category
+              ) as Item[]
+            }
+          />
+        ))}
+      </div>
+      <CreateItemButton
+        onClick={() => {
+          setIsShowing(true);
+        }}
+      />
+      <SideDrawer isShowing={isShowing}>
+        <CreateItemForm
+          onClose={() => {
+            setIsShowing(false);
+          }}
+          collectionRef={itemsCollection}
+          doesItemExist={checkIfItemExists}
         />
-      ))}
-    </div>
+      </SideDrawer>
+    </>
   );
 };
 
