66d97712c14d7f636c4bc4ab5d86478abbfc688e
Avishay Kuperman-Koren <justavishay@gmail.com>
2023-06-19 18:25:58 +0300

    feat: layout the shopping list component

---
+++ src/components/list/ListCategory.tsx
@@ -0,0 +1,9 @@
+import ListItem from "./ListItem.interface";
+
+interface Props {
+  items: ListItem[];
+}
+
+const ListCategory: React.FC<Props> = () => <div></div>;
+
+export default ListCategory;
--- src/components/list/ShoppingList.tsx
+++ src/components/list/ShoppingList.tsx
@@ -1,11 +1,31 @@
+import { useContext } from "react";
+import ShoppingListContext from "./ShoppingListContext";
 import SideCard from "../general/SideCard";
+import categories from "../items/categories";
+import ListCategory from "./ListCategory";
+import Input from "../general/Input";
 
 interface Props {
   onClose(): any;
 }
 
 const ShoppingList: React.FC<Props> = ({ onClose }) => {
-  return <SideCard className="bg-[#e6d8c8] px-12 py-6"></SideCard>;
+  const { items, name } = useContext(ShoppingListContext);
+
+  return (
+    <SideCard className="bg-[#e6d8c8] px-12 py-6">
+      <div>{/* TODO: Make the Add item cta */}</div>
+      <h4>{name}</h4>
+      <div>
+        {categories.map((category) => (
+          <ListCategory
+            items={items.filter((item) => item.category === category)}
+          />
+        ))}
+      </div>
+      <div>{/* name input */}</div>
+    </SideCard>
+  );
 };
 
 export default ShoppingList;
