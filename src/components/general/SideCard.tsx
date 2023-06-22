4ef506325c04e4baf6a6b949842fc8dca1b37a47
RealByter <justavishay@gmail.com>
2023-06-22 22:46:43 +0300

    fix: adjust the SideCard width

--- src/components/general/SideCard.tsx
+++ src/components/general/SideCard.tsx
@@ -5,7 +5,7 @@ interface Props {
 const SideCard: React.FC<Props> = ({ children, className }) => (
   <div
     className={
-      "h-screen w-[calc(100vw-56px)] max-w-md sm:w-[calc(100vw-72px)] md:w-96" +
+      "h-screen w-[calc(100vw-56px)] max-w-sm sm:w-[calc(100vw-56px)] md:w-96" +
       " " +
       className
     }
